28d935d7b5f73547782d51d2ac237f1da25b987d
xXPakoGamer10Xx <73171670+xXPakoGamer10Xx@users.noreply.github.com>
2023-05-19 02:42:37 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ links.forEach(link => {
     target.scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+function convertirNotacion() {
+  // Obtener los valores ingresados por el usuario
+  var numero = parseFloat(document.getElementById('numero').value);
+  var potencia = parseInt(document.getElementById('potencia').value);
+
+  // Realizar la conversión a notación científica
+  var resultadoNotacion = numero + ' x 10^' + potencia;
+  var resultadoNumerico = numero * Math.pow(10, potencia);
+
+  // Redondear el resultado numérico al número exacto de decimales según la potencia
+  var decimales = Math.abs(potencia);
+  resultadoNumerico = resultadoNumerico.toFixed(decimales);
+
+  // Mostrar la notación y el resultado en los elementos <span>
+  document.getElementById('notacion').textContent = "Notación científica: " + resultadoNotacion;
+  document.getElementById('resultado').textContent = resultadoNumerico;
+}
